30d42c296180b1b50db3982bcb6926b923b93460
akshaybabu <anthoorveettilakshaybabu@gmail.com>
2023-03-20 18:30:35 +0530

    my first maven commit

---
+++ src/main/java/my_app/myapp/App.java
@@ -0,0 +1,13 @@
+package my_app.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
